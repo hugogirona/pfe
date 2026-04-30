7892e1df29352af11c275f26f304de09cccad1c7
Hugo Girona <hugo.girona@student.hepl.be>
2026-04-30 12:02:31 +0200

    feat(instrument-seeder): seed 9 instruments idempotently

---
+++ giggr/database/seeders/InstrumentSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Instrument;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class InstrumentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $instruments = [
+            'Guitare', 'Basse', 'Batterie', 'Clavier', 'Violon',
+            'Chant', 'Saxophone', 'Trompette', 'Percussions',
+        ];
+
+        foreach ($instruments as $name) {
+            Instrument::firstOrCreate(
+                ['slug' => Str::slug($name)],
+                ['name' => $name],
+            );
+        }
+    }
+}
