6c0b8f0c9e8730cfdf69006c39499d7f07170881
Hugo Girona <hugo.girona@student.hepl.be>
2026-04-30 16:58:12 +0200

    feat(profile): add profiles + pivot migrations, model, factory and User::profile() relation

---
+++ giggr/database/factories/ProfileFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\ProfileStatus;
+use App\Models\Genre;
+use App\Models\Instrument;
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Profile>
+ */
+class ProfileFactory extends Factory
+{
+    protected $model = Profile::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id'          => User::factory(),
+            'bio'              => fake()->paragraph(),
+            'birth_date'       => fake()->dateTimeBetween('-55 years', '-18 years')->format('Y-m-d'),
+            'avatar_path'      => null,
+            'status'           => fake()->randomElement(ProfileStatus::cases()),
+            'experience_years' => fake()->numberBetween(0, 30),
+        ];
+    }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Profile $profile) {
+            $instruments = Instrument::inRandomOrder()->limit(fake()->numberBetween(1, 3))->get();
+            $genres      = Genre::inRandomOrder()->limit(fake()->numberBetween(1, 3))->get();
+
+            if ($instruments->isNotEmpty()) {
+                $profile->instruments()->sync($instruments->pluck('id'));
+            }
+            if ($genres->isNotEmpty()) {
+                $profile->genres()->sync($genres->pluck('id'));
+            }
+        });
+    }
+}
