687d8b938cef5d3bd0c9c16d3e43b6c938cd74e6
Hugo Girona <hugo.girona@student.hepl.be>
2026-04-30 15:34:55 +0200

    updated base test to ignore frontend assets

--- giggr/tests/Feature/ExampleTest.php
+++ giggr/tests/Feature/ExampleTest.php
@@ -1,6 +1,10 @@
 <?php
 
+namespace Tests\Feature;
+
 test('the application returns a successful response', function () {
+    $this->withoutVite();
+
     $response = $this->get('/');
 
     $response->assertStatus(200);
