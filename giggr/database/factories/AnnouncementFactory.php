e5cb814c09fc0e212a23e5718b2a14e6c8626399
Hugo Girona <hugo.girona@student.hepl.be>
2026-04-30 19:40:53 +0200

    feat(announcement): add announcements + pivot migrations, model, scopes and factory

---
+++ giggr/database/factories/AnnouncementFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\AnnouncementStatus;
+use App\Enums\AnnouncementType;
+use App\Models\Announcement;
+use App\Models\City;
+use App\Models\Genre;
+use App\Models\Instrument;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Announcement>
+ */
+class AnnouncementFactory extends Factory
+{
+    protected $model = Announcement::class;
+
+    public function definition(): array
+    {
+        $hasExpiry = fake()->boolean(50);
+
+        return [
+            'user_id'     => User::factory(),
+            'city_id'     => City::factory(),
+            'title'       => fake()->sentence(5),
+            'description' => fake()->paragraph(),
+            'type'        => fake()->randomElement(AnnouncementType::cases()),
+            'status'      => AnnouncementStatus::Open,
+            'expires_at'  => $hasExpiry ? now()->addDays(fake()->numberBetween(7, 30)) : null,
+        ];
+    }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Announcement $announcement) {
+            $instruments = Instrument::inRandomOrder()->limit(fake()->numberBetween(1, 3))->get();
+            $genres      = Genre::inRandomOrder()->limit(fake()->numberBetween(1, 3))->get();
+
+            if ($instruments->isNotEmpty()) {
+                $announcement->instruments()->sync($instruments->pluck('id'));
+            }
+            if ($genres->isNotEmpty()) {
+                $announcement->genres()->sync($genres->pluck('id'));
+            }
+        });
+    }
+}
