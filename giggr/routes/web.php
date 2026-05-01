2d57c59f95138835d53edc73b05ad246ac94d0e3
Hugo Girona <hugo.girona@student.hepl.be>
2026-05-01 15:29:37 +0200

    implemented auth middleware for unauth users

--- giggr/routes/web.php
+++ giggr/routes/web.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::group([
-    'prefix'     => LaravelLocalization::setLocale(),
+    'prefix' => LaravelLocalization::setLocale(),
     'middleware' => ['localizationRedirect', 'localeViewPath'],
 ], function () {
 
     Route::livewire('/', 'pages::home.index')->name('home');
     Route::livewire('/explorer', 'pages::explore.index')->name('explore');
-    Route::livewire('/profil/{id}', 'pages::profile.index')->name('profile');
-    Route::livewire('/annonces/{id}', 'pages::announcement.index')->name('announcement');
+    Route::livewire('/profil/{id}', 'pages::profile.index')->name('profile')->middleware('auth');
+    Route::livewire('/annonces/{id}', 'pages::announcement.index')->name('announcement')->middleware('auth');
     Route::livewire('/contact', 'pages::contact.index')->name('contact');
     Route::livewire('/register', 'pages::auth.register')->name('register');
     Route::livewire('/login', 'pages::auth.login')->name('login');
