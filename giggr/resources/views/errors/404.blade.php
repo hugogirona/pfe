8cb17d96fc2778637712783adab4547fa464faee
Hugo Girona <hugo.girona@student.hepl.be>
2026-06-10 04:28:33 +0200

    removed font preload

--- giggr/resources/views/errors/404.blade.php
+++ giggr/resources/views/errors/404.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 — {{ config('app.name') }}</title>
-    <link rel="preload" href="{{ Vite::asset('resources/fonts/dm-sans-regular.woff2') }}" as="font" type="font/woff2" crossorigin>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
