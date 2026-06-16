b66b0d97b1c625062aeacbefaa1ef3e0ddf858b2
Hugo Girona <hugo.girona@student.hepl.be>
2026-06-16 11:35:44 +0200

    fix(nav) improved markup

--- giggr/resources/views/components/nav/links.blade.php
+++ giggr/resources/views/components/nav/links.blade.php
@@ -1,5 +1,11 @@
-<div class="hidden md:flex items-center gap-8">
-    <x-nav.link href="{{ route('home') }}">{{ __('nav.home') }}</x-nav.link>
-    <x-nav.link href="{{ route('explore') }}" :exact="false">{{ __('nav.explore') }}</x-nav.link>
-    <x-nav.link href="{{ route('contact') }}">{{ __('nav.contact') }}</x-nav.link>
-</div>
+<ul class="hidden md:flex items-center gap-8">
+    <li>
+        <x-nav.link href="{{ route('home') }}">{{ __('nav.home') }}</x-nav.link>
+    </li>
+    <li>
+        <x-nav.link href="{{ route('explore') }}" :exact="false">{{ __('nav.explore') }}</x-nav.link>
+    </li>
+    <li>
+        <x-nav.link href="{{ route('contact') }}">{{ __('nav.contact') }}</x-nav.link>
+    </li>
+</ul>
